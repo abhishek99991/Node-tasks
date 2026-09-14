38c6e4aa4845bd9ba5f35e57ef9dd83c916f0482
harishtrank <harish@tranktechies.com>
2026-09-14 11:33:27 +0530

    first new

---
+++ middlewarefn.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+// Custom middleware
+function addUser(req, res, next) {
+  req.user = "Guest";
+  next();
+}
+
+// Apply middleware to /welcome
+app.get("/welcome", addUser, (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
